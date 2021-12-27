aa5ce693260cd421f70809a257387b9adc324bc3
izuhara2 <83947847+izuhara2@users.noreply.github.com>
2021-12-27 16:21:11 +0900

    main

---
+++ pokemonDB/delete.js
@@ -0,0 +1,17 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('test2.db');
+
+  let sql = `
+delete from pokemondb where id=4;
+`
+
+
+db.serialize(() => {
+  db.run(sql, (error, row) => {
+    if (error) {
+      console.log('Error: ', error);
+      return;
+    }
+    console.log("データを削除しました");
+  });
+});
---
+++ pokemonDB/schema.js
@@ -0,0 +1,19 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('test2.db');
+
+let sql = `
+select name,sql from sqlite_master where type='table';
+`;
+
+db.serialize( () => {
+	db.each( sql, (error, row) => {
+		if(error) {
+			console.log('Error: ', error );
+			return;
+		}
+		console.log( "pokemondb : " + row.name );
+    console.log( "pokemondb : " + row.type );
+		console.log( "Schema : " + row.sql );
+		console.log();
+	});
+});
